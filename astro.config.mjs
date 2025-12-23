626c52f4480340fa65f659f3c14f27b5fd0fa3cf
Luca Fuster <luca.fuster@telecom-sudparis.eu>
2025-12-24 00:24:50 +0100

    Fix: Remove React from config to solve build error

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  // Remplace par ton URL exacte
   site: 'https://lucafuster.github.io',
-  // Ajoute le nom de ton dépôt ici
-  base: '/lucafuster', 
+  base: '/lucafuster.github.io', 
+  
   integrations: [tailwind()],
 });
